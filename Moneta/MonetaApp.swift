161938c861a98107971c321f2b1c6d852a502b3d
Cyrus Melchor <kyrostheelder@icloud.com>
2021-04-11 18:02:24 -0700

    Initial Commit

---
+++ Moneta/MonetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonetaApp.swift
+//  Moneta
+//
+//  Created by hba on 4/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MonetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
